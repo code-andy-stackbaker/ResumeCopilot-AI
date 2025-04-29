c2cd019fe8cbae3ee481addc0c3bb643f4510ed2
code_andy_stackbaker <code.andy.stackbaker@outlook.com>
2025-04-29 15:26:12 -0500

    prepare the code for fine tuning the bert-base model with classification

--- app/fine_tune_lora_project/train.py
+++ app/fine_tune_lora_project/train.py
@@ -1,6 +1,11 @@
 import torch
 from datasets import load_dataset
-from transformers import AutoModelForSequenceClassification, AutoTokenizer, Trainer, TrainingArguments
+from transformers import (
+    AutoTokenizer,
+    AutoModelForSequenceClassification,
+    TrainingArguments,
+    Trainer
+)
 from peft import LoraConfig, get_peft_model, TaskType
 import pandas as pd
 import json
@@ -24,11 +29,53 @@ lora_config = LoraConfig(
 
 model = get_peft_model(base_model, lora_config)
 print("the model peft:", model.print_trainable_parameters())
-
 dataset = load_dataset("csv", data_files={"train": "datasets/train.csv"})
 
 # Optional: split 80% train, 20% test
 dataset = dataset["train"].train_test_split(test_size=0.2)
 
-print("the datasets:", dataset)
+#tokenize the dataset
 
+
+def tokenize_function(example):
+  return tokenizer(example["text"], truncation = True, padding = "max_length")
+
+tokenized_dataset = dataset.map(tokenize_function, batched = True)
+
+print("Using device:", torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"))
+
+args_custom = TrainingArguments(
+  output_dir="./results",
+  evaluation_strategy="epoch",
+  save_strategy="epoch",
+  learning_rate=2e-4,
+  per_device_train_batch_size=2,
+  per_device_eval_batch_size=2,
+  gradient_accumulation_steps=2,
+  num_train_epochs=3,
+  weight_decay=0.01,
+  logging_dir="./logs",
+  logging_steps=10,
+  no_cuda=False
+)
+
+
+# 7. Create Trainer
+trainer = Trainer(
+    model=model,
+    args=args_custom,
+    train_dataset=tokenized_dataset["train"],
+    eval_dataset=tokenized_dataset["test"],
+)
+
+
+
+print("the datasets:", dataset)
+print("the tokenized dataset", tokenized_dataset)
+
+
+trainer.train()
+
+# 9. Save LoRA adapter only (not full model)
+model.save_pretrained("./lora_adapter")
+tokenizer.save_pretrained("./lora_adapter")
