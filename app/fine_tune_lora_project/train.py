e200639ad9dfe1fa6302a40d1dc473cb57322150
code_andy_stackbaker <code.andy.stackbaker@outlook.com>
2025-04-29 05:48:10 -0500

    add the files

--- app/fine_tune_lora_project/train.py
+++ app/fine_tune_lora_project/train.py
@@ -2,6 +2,7 @@ import torch
 from datasets import load_dataset
 from transformers import AutoModelForSequenceClassification, AutoTokenizer, Trainer, TrainingArguments
 from peft import LoraConfig, get_peft_model, TaskType
+import pandas as pd
 import json
 
 with open("lora_config.json", "r") as f:
@@ -10,7 +11,7 @@ with open("lora_config.json", "r") as f:
 
 model_name = "bert-base-uncased"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
-base_model = AutoModelForSequenceClassification.from_pretrained(model_name, num_label=2)
+base_model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=2)
 
 lora_config = LoraConfig(
   r = lora_params["r"],
@@ -20,4 +21,14 @@ lora_config = LoraConfig(
   bias = lora_params["bias"],
   task_type=TaskType.SEQ_CLS
 )
+
+model = get_peft_model(base_model, lora_config)
+print("the model peft:", model.print_trainable_parameters())
+
+dataset = load_dataset("csv", data_files={"train": "datasets/train.csv"})
+
+# Optional: split 80% train, 20% test
+dataset = dataset["train"].train_test_split(test_size=0.2)
+
+print("the datasets:", dataset)
 
