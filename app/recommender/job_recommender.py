b267117cf312ed810bd1c92e62cbd6f8250ec1f9
code_andy_stackbaker <code.andy.stackbaker@outlook.com>
2025-05-01 17:09:10 -0500

    remove uneccessary files

--- app/recommender/job_recommender.py
+++ app/recommender/job_recommender.py
@@ -5,42 +5,42 @@ import numpy as np
 import logging
 
 class JobRecommender:
-    def __init__(self, faiss_index_path, metadata_path, model_name="all-MiniLM-L6-v2", top_k=5):
-      self.top_k = top_k
-      self.model = SentenceTransformer(model_name)
-      try:
-        self.index = faiss.read_index(faiss_index_path)
-      except Exception as e:
-        logging.error(f"Failed to load FAISS index: {e}")
-        raise
-      try: 
-        self.metadata = pd.read_csv(metadata_path)
-      except Exception as e:
-        logging.error(f"Failed to load FAISS index: {e}")
-        raise
+  def __init__(self, faiss_index_path, metadata_path, model_name="all-MiniLM-L6-v2", top_k=5):
+    self.top_k = top_k
+    self.model = SentenceTransformer(model_name)
+    try:
+      self.index = faiss.read_index(faiss_index_path)
+    except Exception as e:
+      logging.error(f"Failed to load FAISS index: {e}")
+      raise
+    try: 
+      self.metadata = pd.read_csv(metadata_path)
+    except Exception as e:
+      logging.error(f"Failed to load FAISS index: {e}")
+      raise
 
-    def recommend(self, resume_text: str):
-      if not resume_text or not isinstance(resume_text, str):
-        raise ValueError("Resume text must be a non-empty string.")
-      
-      try:
-        resume_vector = self.model.encode([resume_text], convert_to_numpy=True)
-        distances, indices = self.index.search(resume_vector, self.top_k)
-      except Exception as e:
-        logging.error(f"FAISS search failed: {e}")
-        return []
+  def recommend(self, resume_text: str):
+    if not resume_text or not isinstance(resume_text, str):
+      raise ValueError("Resume text must be a non-empty string.")
     
-      results = []
-      print("the distances", distances)
-      for i, idx in enumerate(indices[0]):
-        job = self.metadata.iloc[idx]["job_desciption"]
-       
-        score = float(distances[0][i])
-        print("the score", score)
-        results.append({
-            "rank": i + 1,
-            "job_description": job,
-            "faiss_score": round(score, 4)
-        })
+    try:
+      resume_vector = self.model.encode([resume_text], convert_to_numpy=True)
+      distances, indices = self.index.search(resume_vector, self.top_k)
+    except Exception as e:
+      logging.error(f"FAISS search failed: {e}")
+      return []
+  
+    results = []
+    print("the distances", distances)
+    for i, idx in enumerate(indices[0]):
+      job = self.metadata.iloc[idx]["job_desciption"]
+      
+      score = float(distances[0][i])
+      print("the score", score)
+      results.append({
+          "rank": i + 1,
+          "job_description": job,
+          "faiss_score": round(score, 4)
+      })
 
-      return results
+    return results
